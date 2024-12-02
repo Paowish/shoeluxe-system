43d27be174b5249c1849bb4d3b28985836a86cd7
Kenethm <caruniaken@gmail.com>
2024-12-03 00:16:02 +0800

    Add Feature, Fix some layouts, Persist cart

--- Shoes/script.js
+++ Shoes/script.js
@@ -1,293 +1,127 @@
-let productItems = [
-            
-    {
-        brandName: "Nike",
-        imgSrc: "img/shoes1.png",
-        description: "AF1 low-top retro classic shoes",
-        price:  2500,
-        priceLabel: "₱ 2,500",
-    },
-    {
-        brandName: "Nike",
-        imgSrc: "img/shoes2.png",
-        description: "AF2 low-top retro classic shoes",
-        price:  2400,
-        priceLabel: "₱ 2,400",
-    },
-    {
-        brandName: "Nike",
-        imgSrc: "img/shoes3.png",
-        description: "AF3 low-top retro classic shoes",
-        price: 2600,
-        priceLabel: "₱ 2,600",
-    },
-    {
-        brandName: "Nike",
-        imgSrc: "img/shoes4.png",
-        description: "AF4 low-top retro classic shoes",
-        price: 2200,
-        priceLabel: "₱ 2,200",
-    },
-    {
-        brandName: "Nike",
-        imgSrc: "img/shoes5.png",
-        description: "AF5 low-top retro classic shoes",
-        price: 2700,
-        priceLabel: "₱ 2,700",
-    },
-    {
-        brandName: "Nike",
-        imgSrc: "img/shoes6.png",
-        description: "AF6 low-top retro classic shoes",
-        price: 2800,
-        priceLabel: "₱ 2,800",
-    },
-    {
-        brandName: "Nike",
-        imgSrc: "img/shoes7.png",
-        description: "AF7 low-top retro classic shoes",
-        price: 2100,
-        priceLabel: "₱ 2,100",
-    },
-    {
-        brandName: "Nike",
-        imgSrc: "img/shoes8.png",
-        description: "AF8 low-top retro classic shoes",
-        price: 2900,
-        priceLabel: "₱ 2,900",
-    },
-    {
-        brandName: "Adidas",
-        imgSrc: "img/adidas1.AVIF",
-        description: "Campus 00s Retro shoeses",
-        price:  1500,
-        priceLabel: "₱ 1,500",
-    },
-    {
-        brandName: "Adidas",
-        imgSrc: "img/adidas2.AVIF",
-        description: "Campus 00s Retro shoeses",
-        price:  1200,
-        priceLabel: "₱ 1,200",
-    },
-    {
-        brandName: "Adidas",
-        imgSrc: "img/adidas3.AVIF",
-        description: "Campus 00s Retro shoeses",
-        price:  1300,
-        priceLabel: "₱ 1,300",
-    },
-    {
-        brandName: "Adidas",
-        imgSrc: "img/adidas4.AVIF",
-        description: "Campus 00s Retro shoeses",
-        price:  1400,
-        priceLabel: "₱ 1,400",
-    },
-    {
-        brandName: "Adidas",
-        imgSrc: "img/adidas5.jpg",
-        description: "Campus 00s Retro shoeses",
-        price:  1600,
-        priceLabel: "₱ 1,600",
-    },
-    {
-        brandName: "Adidas",
-        imgSrc: "img/adidas6.jpg",
-        description: "Campus 00s Retro shoeses",
-        price: 1700,
-        priceLabel: "₱ 1,700",
-    },
-    {
-        brandName: "Adidas",
-        imgSrc: "img/adidas7.jpg",
-        description: "Campus 00s Retro shoeses",
-        price:  1800,
-        priceLabel: "₱ 1,800",
-    },
-    {
-        brandName: "Adidas",
-        imgSrc: "img/adidas8.jpg",
-        description: "Campus 00s Retro shoeses",
-        price:  1900,
-        priceLabel: "₱ 1,900",
-    },
-    {
-        brandName: "Converse",
-        imgSrc: "img/converse1.jpg",
-        description: "Converse Fashion",
-        price:  2500,
-        priceLabel: "₱ 2,500",
-    },
-    {
-        brandName: "Converse",
-        imgSrc: "img/converse2.jpg",
-        description: "Converse Fashion",
-        price:  2300,
-        priceLabel: "₱ 2,300",
-    },
-    {
-        brandName: "Converse",
-        imgSrc: "img/converse3.jpg",
-        description: "Converse Fashion",
-        price:  2100,
-        priceLabel: "₱ 2,100",
-    },
-    {
-        brandName: "Converse",
-        imgSrc: "img/converse4.jpg",
-        description: "Converse Fashion",
-        price:  2700,
-        priceLabel: "₱ 2,700",
-    },
-    {
-        brandName: "Converse",
-        imgSrc: "img/converse5.jpg",
-        description: "Converse Fashion",
-        price:  3500,
-        priceLabel: "₱ 3,500",
-    },
-    {
-        brandName: "Converse",
-        imgSrc: "img/converse6.jpg",
-        description: "Converse Fashion",
-        price:  3300,
-        priceLabel: "₱ 3,300",
-    },
-    {
-        brandName: "Converse",
-        imgSrc: "img/converse7.jpg",
-        description: "Converse Fashion",
-        price:  2500,
-        priceLabel: "₱ 2,500",
-    },
-    {
-        brandName: "Converse",
-        imgSrc: "img/converse8.jpg",
-        description: "Converse Fashion",
-        price:  2500,
-        priceLabel: "₱ 2,500",
-    },
-    {
-        brandName: "Puma",
-        imgSrc: "img/puma1.jpg",
-        description: "Puma Fashion",
-        price:  2700,
-        priceLabel: "₱ 2,700",
-    },
-    {
-        brandName: "Puma",
-        imgSrc: "img/puma2.jpg",
-        description: "Puma Fashion",
-        price:  3500,
-        priceLabel: "₱ 3,500",
-    },
-    {
-        brandName: "Puma",
-        imgSrc: "img/puma3.jpg",
-        description: "Puma Fashion",
-        price:  3700,
-        priceLabel: "₱ 3,700",
-    },
-    {
-        brandName: "Puma",
-        imgSrc: "img/puma4.jpg",
-        description: "Puma Fashion",
-        price:  2500,
-        priceLabel: "₱ 2,500",
-    },
-    {
-        brandName: "Puma",
-        imgSrc: "img/puma5.jpg",
-        description: "Puma Fashion",
-        price:  2600,
-        priceLabel: "₱ 2,600",
-    },
-    {
-        brandName: "Puma",
-        imgSrc: "img/puma6.jpg",
-        description: "Puma Fashion",
-        price:  2100,
-        priceLabel: "₱ 2,100",
-    },
-    {
-        brandName: "Puma",
-        imgSrc: "img/puma7.jpg",
-        description: "Puma Fashion",
-        price:  2500,
-        priceLabel: "₱ 2,500",
-    },
-    {
-        brandName: "Puma",
-        imgSrc: "img/puma8.jpg",
-        description: "Puma Fashion",
-        price:  2800,
-        priceLabel: "₱ 2,800",
-    },
-    {
-        brandName: "World Balance",
-        imgSrc: "img/world1.jpg",
-        description: "Balance Fashion",
-        price:  5500,
-        priceLabel: "₱ 5,500",
-    },
-    {
-        brandName: "World Balance",
-        imgSrc: "img/world2.jpg",
-        description: "Balance Fashion",
-        price:  3500,
-        priceLabel: "₱ 3,500",
-    },
-    {
-        brandName: "World Balance",
-        imgSrc: "img/world3.jpg",
-        description: "Balance Fashion",
-        price:  2500,
-        priceLabel: "₱ 2,500",
-    },
-    {
-        brandName: "World Balance",
-        imgSrc: "img/world4.jpg",
-        description: "Balance Fashion",
-        price:  1500,
-        priceLabel: "₱ 1,500",
-    },
-    {
-        brandName: "World Balance",
-        imgSrc: "img/world5.jpg",
-        description: "Balance Fashion",
-        price:  2300,
-        priceLabel: "₱ 2,300",
-    },
-    {
-        brandName: "World Balance",
-        imgSrc: "img/world6.jpg",
-        description: "Balance Fashion",
-        price:  2500,
-        priceLabel: "₱ 2,500",
-    },
-    {
-        brandName: "World Balance",
-        imgSrc: "img/world7.jpg",
-        description: "Balance Fashion",
-        price:  2700,
-        priceLabel: "₱ 2,700",
-    },
-    {
-        brandName: "World Balance",
-        imgSrc: "img/world8.jpg",
-        description: "Balance Fashion",
-        price:  2100,
-        priceLabel: "₱ 2,100",
-    }
-    
-];
+let productItems = [];
+
+let filteredBrands = [];
 
 let selectedBrand = "";
 
-let filteredBrands = productItems;
-
 let addedToCart = [];
+
+loadProducts();
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.addEventListener("scroll", function(){
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        }
+    });
+}
+
+const observer = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const modal = document.getElementById('myModal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeBtn = document.querySelector('.close-btn');
+
+openModalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+  if (addedToCart.length > 0){
+    document.getElementById('checkout').style.display = "block";
+  }else {
+    document.getElementById('checkout').style.display = "none";
+  }
+  displayCart();
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+function addProduct() {
+    let filteredBrands = JSON.parse(localStorage.getItem('products'));
+    filteredBrands.push({
+        brandName: document.getElementById("brandName-sm").value,
+        description: document.getElementById("description-sm").value,
+        price: document.getElementById("price-sm").value,
+        priceLabel: document.getElementById("priceLabel-sm").value,
+        imgSrc: document.getElementById("image-sm").value
+    });
+    localStorage.setItem("products",JSON.stringify(filteredBrands));
+    window.location.href = "http://127.0.0.1:5500/Shoes/index.html#products"
+}
+
+function loadCart() {
+    addedToCart = JSON.parse(localStorage.getItem("cart")) || [];
+    document.getElementById('countLabel').innerHTML = addedToCart.length;
+}
+
+function updateCart() {
+    localStorage.setItem("cart", JSON.stringify(addedToCart));
+    document.getElementById('countLabel').innerHTML = addedToCart.length;
+}
+
+function clearCart() {
+    localStorage.removeItem('cart');
+    localStorage.clear();
+    addedToCart = [];
+    document.getElementById('countLabel').innerHTML = 0;
+}
+
+function loadProducts() {
+    loadCart();
+
+    if(localStorage.getItem('products')) {
+        let jsonData = JSON.parse(localStorage.getItem('products'));
+        productItems = jsonData;
+        filteredBrands = jsonData;
+        filterBrand();
+    } else {
+        fetch('http://127.0.0.1:5500/Shoes/products.txt')
+            .then((response) => {
+                if (!response.ok) {
+                throw new Error('Failed to fetch the file');
+                }
+                return response.text();
+            })
+            .then((data) => {
+                const jsonData = JSON.parse(data); // Parse as JSON
+                productItems = jsonData;
+                filteredBrands = jsonData;
+                localStorage.setItem('products', data);
+                filterBrand();
+            })
+            .catch((error) => {
+                console.error('Error accessing file:', error);
+            });
+    }
+}
 
 function displayProducts(products){
     var product = document.getElementById("product");
@@ -319,102 +153,46 @@ function filterBrand(){
     displayProducts(filteredBrands);
 }
 
-
-filterBrand();
-
-window.addEventListener("scroll", function(){
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 0);
-});
-
-
 function addToCart(brandName, imgSrc, description, price, priceLabel) {
-    console.log(price); // all of the array objects
-
     let product = {
         quantity:1,
         brandName: brandName,
         imgSrc: imgSrc,
         description: description,
         price:  price,
-        priceLabel: priceLabel,
+        priceLabel: priceLabel
     }
     addedToCart.push(product);
-    document.getElementById('countLabel').innerHTML = addedToCart.length;
-    console.log(addedToCart);
+    updateCart();
 }
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-sections.forEach(sec => {
-let top = window.scrollY;
-let offset = sec.offsetTop - 100;
-let height = sec.offsetHeight;
-let id = sec.getAttribute('id');
-
-if(top >= offset && top < offset + height){
-    navLinks.forEach(links =>{
-        links.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-    });
-};
-});
-};
-
-const observer = new IntersectionObserver ((entries) => {
-entries.forEach((entry) => {
-console.log(entry)
-if (entry.isIntersecting){
-    entry.target.classList.add('show');
-}else{
-    entry.target.classList.remove('show')
-}
-});
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
-
-const modal = document.getElementById('myModal');
-const openModalBtn = document.getElementById('openModalBtn');
-const closeBtn = document.querySelector('.close-btn');
-
-openModalBtn.addEventListener('click', () => {
-  modal.style.display = 'block';
-  if (addedToCart.length > 0){
-    document.getElementById('checkout').style.display = "block";
-  }else {
-    document.getElementById('checkout').style.display = "none";
-  }
-  displayCart();
-
-});
 
 function displayCart(){
     let items = document.getElementById('items-container');
     items.innerHTML = "";
     for (let x = 0; x < addedToCart.length; x++){
-    items.innerHTML +='<div class="items">' +
-    '<h3><img src="'+ addedToCart[x].imgSrc+'" width="45px"><h3 class="items2"><span class="brandName">'+addedToCart[x].brandName+'</span> <br> <span class="description-cart">'+ addedToCart[x].description+'</span></h3>'+
-    '<h3 class="pricing">'+ addedToCart[x].priceLabel+'</h3>' +
-    '<h3 class="quantity"><i onclick="decreaseQuantity('+addedToCart[x].quantity+', '+x+')" class="lessthan fa-solid fa-less-than"></i><span id="increaseNum'+x+'">'+addedToCart[x].quantity+'</span><i onclick="increaseQuantity('+addedToCart[x].quantity+', '+x+')" class=" greaterthan fa-solid fa-greater-than"></i></h3>'+
-    '<h3 class="total">'+ addedToCart[x].price*addedToCart[x].quantity+'</h3>'+
-    
-  '</div>'
-    
-
-  }
-
+        items.innerHTML += '<tr>' +
+            '<td class="items-cart t-data">'+
+                '<img class="item-img" src="'+ addedToCart[x].imgSrc+'" width="45px">' +
+                '<div class="brand-desc">' +
+                    '<span class="brandName">'+addedToCart[x].brandName+'</span> <br>' + 
+                    '<span class="description-cart">'+ addedToCart[x].description+'</span></td>' +
+                '</div>' +
+            '<td class="t-data"><h3 class="pricing">'+ addedToCart[x].priceLabel+'</h3></td>' +
+            '<td class="t-data"><h3 class="quantity"><i onclick="decreaseQuantity('+addedToCart[x].quantity+', '+x+')" class="lessthan fa-solid fa-less-than"></i><span id="increaseNum'+x+'">'+addedToCart[x].quantity+'</span><i onclick="increaseQuantity('+addedToCart[x].quantity+', '+x+')" class=" greaterthan fa-solid fa-greater-than"></i></h3></td>' +
+            '<td class="t-data"><h3 class="total">'+ addedToCart[x].price*addedToCart[x].quantity+'</h3></td>' +
+        '</tr>';
+    }
 }
+
 function decreaseQuantity(quantity, index){
-    console.log(quantity, index);
-    if(quantity -1 == 0){
+    
+    if(quantity-1 == 0){
         addedToCart.splice(index, 1);
-        document.getElementById('checkout').style.display = "none";
-        document.getElementById('countLabel').innerHTML = addedToCart.length;
-    }else {
+        if(addedToCart.length == 0) {
+            document.getElementById('checkout').style.display = "none";
+        }
+        updateCart();
+    } else {
         addedToCart[index].quantity = quantity-1;
     }
     displayCart();
@@ -429,15 +207,11 @@ function increaseQuantity(quantity, index){
 function checkoutButton(){
     let checkout = document.getElementById('checkout')
     if (checkout){
+        window.location.href="http://127.0.0.1:5500/Shoes/feedback.html";
         alert("Thank you for checking out!");
+        clearCart();
         closeBtn.click();
-        window.location.href="http://127.0.0.1:5500/Shoes/index.html#home";
     }
 }
-
-
-closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
 
 
