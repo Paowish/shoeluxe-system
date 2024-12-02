7b85444ebf2c0099604764dabf5af00586d93f89
Paowish <paolocarunia139@gmail.com>
2024-12-02 21:56:55 +0800

    update all

--- Shoes/script.js
+++ Shoes/script.js
@@ -3,280 +3,345 @@ let productItems = [
     {
         brandName: "Nike",
         imgSrc: "img/shoes1.png",
-        description: "AF1 low-top retro classic sneaks pure white For Men",
-        price: "₱ 1,500",
+        description: "AF1 low-top retro classic shoes",
+        price:  2500,
+        priceLabel: "₱ 2,500",
     },
     {
         brandName: "Nike",
         imgSrc: "img/shoes2.png",
-        description: "NAir Jodan High quality shoes, fashionable casual",
-        price: "₱ 1,500",
+        description: "AF2 low-top retro classic shoes",
+        price:  2400,
+        priceLabel: "₱ 2,400",
     },
     {
         brandName: "Nike",
         imgSrc: "img/shoes3.png",
-        description: "Fashion aF1 White Shoes Low cut Sneakers shoes",
-        price: "₱ 1,500",
+        description: "AF3 low-top retro classic shoes",
+        price: 2600,
+        priceLabel: "₱ 2,600",
     },
     {
         brandName: "Nike",
         imgSrc: "img/shoes4.png",
-        description: "Fragment Design Travis Scott shoes original",
-        price: "₱ 1,500",
+        description: "AF4 low-top retro classic shoes",
+        price: 2200,
+        priceLabel: "₱ 2,200",
     },
     {
         brandName: "Nike",
         imgSrc: "img/shoes5.png",
-        description: "Shoes_nike_af1 Black, Blue Standard Air Shoes, Nike",
-        price: "₱ 1,500",
+        description: "AF5 low-top retro classic shoes",
+        price: 2700,
+        priceLabel: "₱ 2,700",
     },
     {
         brandName: "Nike",
         imgSrc: "img/shoes6.png",
-        description: "AF1 For women's and men's shoes Size 36-45 UA",
-        price: "₱ 1,500",
+        description: "AF6 low-top retro classic shoes",
+        price: 2800,
+        priceLabel: "₱ 2,800",
     },
     {
         brandName: "Nike",
         imgSrc: "img/shoes7.png",
-        description: "Nike M2K Tekn Running Shoes black and white",
-        price: "₱ 1,500",
+        description: "AF7 low-top retro classic shoes",
+        price: 2100,
+        priceLabel: "₱ 2,100",
     },
     {
         brandName: "Nike",
         imgSrc: "img/shoes8.png",
-        description: "Nike AF1 STUSSY FOSSIL for Men&Women by Xian",
-        price: "₱ 1,500",
+        description: "AF8 low-top retro classic shoes",
+        price: 2900,
+        priceLabel: "₱ 2,900",
     },
     {
         brandName: "Adidas",
         imgSrc: "img/adidas1.AVIF",
-        description: "Campus 00s Retro Trend Casual Bread Shoes korn",
-        price: "₱ 1,500",
+        description: "Campus 00s Retro shoeses",
+        price:  1500,
+        priceLabel: "₱ 1,500",
     },
     {
         brandName: "Adidas",
         imgSrc: "img/adidas2.AVIF",
-        description: "Sneakers Low Top Skateboard Shoes For Men",
-        price: "₱ 1,500",
+        description: "Campus 00s Retro shoeses",
+        price:  1200,
+        priceLabel: "₱ 1,200",
     },
     {
         brandName: "Adidas",
         imgSrc: "img/adidas3.AVIF",
-        description: "Adidas AF1 STUSSY FOSSIL for Men&Women by Xian",
-        price: "₱ 1,500",
+        description: "Campus 00s Retro shoeses",
+        price:  1300,
+        priceLabel: "₱ 1,300",
     },
     {
         brandName: "Adidas",
         imgSrc: "img/adidas4.AVIF",
-        description: "Unisex Sneakers Low Cut Skateboard Shoes For Men",
-        price: "₱ 1,500",
+        description: "Campus 00s Retro shoeses",
+        price:  1400,
+        priceLabel: "₱ 1,400",
     },
     {
         brandName: "Adidas",
         imgSrc: "img/adidas5.jpg",
-        description: "Adidas 100%Originals Superstar Low Top Sneakers",
-        price: "₱ 1,500",
+        description: "Campus 00s Retro shoeses",
+        price:  1600,
+        priceLabel: "₱ 1,600",
     },
     {
         brandName: "Adidas",
         imgSrc: "img/adidas6.jpg",
-        description: "Adidas Alpha bounce Running shoes for Women",
-        price: "₱ 1,500",
+        description: "Campus 00s Retro shoeses",
+        price: 1700,
+        priceLabel: "₱ 1,700",
     },
     {
         brandName: "Adidas",
         imgSrc: "img/adidas7.jpg",
-        description: "Adidas Samba OG White Black Gray Unisex Shoes",
-        price: "₱ 1,500",
+        description: "Campus 00s Retro shoeses",
+        price:  1800,
+        priceLabel: "₱ 1,800",
     },
     {
         brandName: "Adidas",
         imgSrc: "img/adidas8.jpg",
-        description: "Adidas Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Campus 00s Retro shoeses",
+        price:  1900,
+        priceLabel: "₱ 1,900",
     },
     {
         brandName: "Converse",
         imgSrc: "img/converse1.jpg",
-        description: "Converse Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Converse Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500",
     },
     {
         brandName: "Converse",
         imgSrc: "img/converse2.jpg",
-        description: "Converse Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Converse Fashion",
+        price:  2300,
+        priceLabel: "₱ 2,300",
     },
     {
         brandName: "Converse",
         imgSrc: "img/converse3.jpg",
-        description: "Converse Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Converse Fashion",
+        price:  2100,
+        priceLabel: "₱ 2,100",
     },
     {
         brandName: "Converse",
         imgSrc: "img/converse4.jpg",
-        description: "Converse Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Converse Fashion",
+        price:  2700,
+        priceLabel: "₱ 2,700",
     },
     {
         brandName: "Converse",
         imgSrc: "img/converse5.jpg",
-        description: "Converse Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Converse Fashion",
+        price:  3500,
+        priceLabel: "₱ 3,500",
     },
     {
         brandName: "Converse",
         imgSrc: "img/converse6.jpg",
-        description: "Converse Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Converse Fashion",
+        price:  3300,
+        priceLabel: "₱ 3,300",
     },
     {
         brandName: "Converse",
         imgSrc: "img/converse7.jpg",
-        description: "Converse Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Converse Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500",
     },
     {
         brandName: "Converse",
         imgSrc: "img/converse8.jpg",
-        description: "Converse Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Converse Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500",
     },
     {
         brandName: "Puma",
         imgSrc: "img/puma1.jpg",
-        description: "Puma Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Puma Fashion",
+        price:  2700,
+        priceLabel: "₱ 2,700",
     },
     {
         brandName: "Puma",
         imgSrc: "img/puma2.jpg",
-        description: "Puma Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Puma Fashion",
+        price:  3500,
+        priceLabel: "₱ 3,500",
     },
     {
         brandName: "Puma",
         imgSrc: "img/puma3.jpg",
-        description: "Puma Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Puma Fashion",
+        price:  3700,
+        priceLabel: "₱ 3,700",
     },
     {
         brandName: "Puma",
         imgSrc: "img/puma4.jpg",
-        description: "Puma Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Puma Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500",
     },
     {
         brandName: "Puma",
         imgSrc: "img/puma5.jpg",
-        description: "Puma Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Puma Fashion",
+        price:  2600,
+        priceLabel: "₱ 2,600",
     },
     {
         brandName: "Puma",
         imgSrc: "img/puma6.jpg",
-        description: "Puma Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Puma Fashion",
+        price:  2100,
+        priceLabel: "₱ 2,100",
     },
     {
         brandName: "Puma",
         imgSrc: "img/puma7.jpg",
-        description: "Puma Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Puma Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500",
     },
     {
         brandName: "Puma",
         imgSrc: "img/puma8.jpg",
-        description: "Puma Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Puma Fashion",
+        price:  2800,
+        priceLabel: "₱ 2,800",
     },
     {
         brandName: "World Balance",
         imgSrc: "img/world1.jpg",
-        description: "Balance Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Balance Fashion",
+        price:  5500,
+        priceLabel: "₱ 5,500",
     },
     {
         brandName: "World Balance",
         imgSrc: "img/world2.jpg",
-        description: "Balance Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Balance Fashion",
+        price:  3500,
+        priceLabel: "₱ 3,500",
     },
     {
         brandName: "World Balance",
         imgSrc: "img/world3.jpg",
-        description: "Balance Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Balance Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500",
     },
     {
         brandName: "World Balance",
         imgSrc: "img/world4.jpg",
-        description: "Balance Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Balance Fashion",
+        price:  1500,
+        priceLabel: "₱ 1,500",
     },
     {
         brandName: "World Balance",
         imgSrc: "img/world5.jpg",
-        description: "Balance Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Balance Fashion",
+        price:  2300,
+        priceLabel: "₱ 2,300",
     },
     {
         brandName: "World Balance",
         imgSrc: "img/world6.jpg",
-        description: "Balance Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Balance Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500",
     },
     {
         brandName: "World Balance",
         imgSrc: "img/world7.jpg",
-        description: "Balance Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Balance Fashion",
+        price:  2700,
+        priceLabel: "₱ 2,700",
     },
     {
         brandName: "World Balance",
         imgSrc: "img/world8.jpg",
-        description: "Balance Fashion Sneakers Rubber Shoes Low Cut Shoes",
-        price: "₱ 1,500",
+        description: "Balance Fashion",
+        price:  2100,
+        priceLabel: "₱ 2,100",
     }
     
 ];
 
+let selectedBrand = "";
 
-function filterBrand(){
-    const selectedBrand = document.getElementById('filter').value;
-    showBrands(selectedBrand);
-}
+let filteredBrands = productItems;
 
-function showBrands(selectedBrand){
-    var filteredBrands = productItems.filter(function(product){
-        return selectedBrand == "" || product.brandName == selectedBrand;
-    });
+let addedToCart = [];
 
+function displayProducts(products){
     var product = document.getElementById("product");
     product.innerHTML = "";
-    for(let x=0; x<filteredBrands.length; x++) {
+    for(let x=0; x<products.length; x++) {
         product.innerHTML += '<div class="items">' +
             '<div class="product-img">' +
-            '<img src='+filteredBrands[x].imgSrc+'>' +
+            '<img src='+products[x].imgSrc+'>' +
             '</div>' +
-            '<h6 class="brandname">'+filteredBrands[x].brandName+'</h6>' +
-            '<span class="description">'+filteredBrands[x].description+'</span>' +
-            '<span class="price">'+filteredBrands[x].price+'</span>' +
-            '<button onclick="addToCart('+x+')" id="cart-btn">Add to Cart</button>' +
+            '<h6 class="brandname">'+products[x].brandName+'</h6>' +
+            '<span class="description">'+products[x].description+'</span>' +
+            '<span class="price">'+products[x].priceLabel+'</span>' +
+            '<button onclick="addToCart(\''+ products[x].brandName +'\', \''+ products[x].imgSrc +'\', \''+ products[x].description +'\', \''+ products[x].price +'\', \''+ products[x].priceLabel +'\')" class="cart-btn">Add to Cart</button>' +
         '</div>';
     }
 }
-showBrands("");
 
+
+function filterBrand(){
+    let search = document.getElementById('find').value;
+    selectedBrand = document.getElementById('filter').value;
+    if(selectedBrand == "all"){
+        selectedBrand = "";
+    }
+    filteredBrands = productItems.filter(function(product){
+        return (selectedBrand == "" || product.brandName == selectedBrand) && (product.brandName.toLowerCase().includes(search.toLowerCase()) || product.description.toLowerCase().includes(search.toLowerCase()));
+    });
+
+    displayProducts(filteredBrands);
+}
+
+
+filterBrand();
 
 window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 });
-function addToCart(productIndex){
-    console.log(productItems[productIndex]);
+
+
+function addToCart(brandName, imgSrc, description, price, priceLabel) {
+    console.log(price); // all of the array objects
+
+    let product = {
+        quantity:1,
+        brandName: brandName,
+        imgSrc: imgSrc,
+        description: description,
+        price:  price,
+        priceLabel: priceLabel,
+    }
+    addedToCart.push(product);
+    document.getElementById('countLabel').innerHTML = addedToCart.length;
+    console.log(addedToCart);
 }
 
 let sections = document.querySelectorAll('section');
@@ -311,3 +376,68 @@ if (entry.isIntersecting){
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const modal = document.getElementById('myModal');
+const openModalBtn = document.getElementById('openModalBtn');
+const closeBtn = document.querySelector('.close-btn');
+
+openModalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+  if (addedToCart.length > 0){
+    document.getElementById('checkout').style.display = "block";
+  }else {
+    document.getElementById('checkout').style.display = "none";
+  }
+  displayCart();
+
+});
+
+function displayCart(){
+    let items = document.getElementById('items-container');
+    items.innerHTML = "";
+    for (let x = 0; x < addedToCart.length; x++){
+    items.innerHTML +='<div class="items">' +
+    '<h3><img src="'+ addedToCart[x].imgSrc+'" width="45px"><h3 class="items2"><span class="brandName">'+addedToCart[x].brandName+'</span> <br> <span class="description-cart">'+ addedToCart[x].description+'</span></h3>'+
+    '<h3 class="pricing">'+ addedToCart[x].priceLabel+'</h3>' +
+    '<h3 class="quantity"><i onclick="decreaseQuantity('+addedToCart[x].quantity+', '+x+')" class="lessthan fa-solid fa-less-than"></i><span id="increaseNum'+x+'">'+addedToCart[x].quantity+'</span><i onclick="increaseQuantity('+addedToCart[x].quantity+', '+x+')" class=" greaterthan fa-solid fa-greater-than"></i></h3>'+
+    '<h3 class="total">'+ addedToCart[x].price*addedToCart[x].quantity+'</h3>'+
+    
+  '</div>'
+    
+
+  }
+
+}
+function decreaseQuantity(quantity, index){
+    console.log(quantity, index);
+    if(quantity -1 == 0){
+        addedToCart.splice(index, 1);
+        document.getElementById('checkout').style.display = "none";
+        document.getElementById('countLabel').innerHTML = addedToCart.length;
+    }else {
+        addedToCart[index].quantity = quantity-1;
+    }
+    displayCart();
+}
+
+function increaseQuantity(quantity, index){
+    console.log(quantity, index);
+    addedToCart[index].quantity = quantity+1;
+    displayCart();
+}
+
+function checkoutButton(){
+    let checkout = document.getElementById('checkout')
+    if (checkout){
+        alert("Thank you for checking out!");
+        closeBtn.click();
+        window.location.href="http://127.0.0.1:5500/Shoes/index.html#home";
+    }
+}
+
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+
