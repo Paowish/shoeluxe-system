52c83ad7b8e90cf2ee67ac442a42453cee304227
Paowish <paolocarunia139@gmail.com>
2024-12-05 16:50:13 +0800

    update all

--- Shoes/script.js
+++ Shoes/script.js
@@ -1,4 +1,259 @@
-let productItems = [];
+let productItems = [
+   
+    {
+        brandName: "Nike",
+        imgSrc: "img/shoes2.png",
+        description: "AF2 low-top retro classic shoes",
+        price:  2400,
+        priceLabel: "₱ 2,400"
+    },
+    {
+        brandName: "Adidas",
+        imgSrc: "img/adidas1.AVIF",
+        description: "Campus 00s Retro shoeses",
+        price:  1500,
+        priceLabel: "₱ 1,500"
+    },
+    {
+        brandName: "Converse",
+        imgSrc: "img/converse2.jpg",
+        description: "Converse Fashion",
+        price:  2300,
+        priceLabel: "₱ 2,300"
+    },
+    {
+        brandName: "Puma",
+        imgSrc: "img/puma1.jpg",
+        description: "Puma Fashion",
+        price:  2700,
+        priceLabel: "₱ 2,700"
+    },
+    {
+        brandName: "World Balance",
+        imgSrc: "img/world1.jpg",
+        description: "Balance Fashion",
+        price:  5500,
+        priceLabel: "₱ 5,500"
+    },
+    {
+        brandName: "Converse",
+        imgSrc: "img/converse1.jpg",
+        description: "Converse Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500"
+    },
+    {
+        brandName: "Nike",
+        imgSrc: "img/shoes6.png",
+        description: "AF6 low-top retro classic shoes",
+        price: 2800,
+        priceLabel: "₱ 2,800"
+    },
+    {
+        brandName: "Adidas",
+        imgSrc: "img/adidas3.AVIF",
+        description: "Campus 00s Retro shoeses",
+        price:  1300,
+        priceLabel: "₱ 1,300"
+    },
+    {
+        brandName: "Puma",
+        imgSrc: "img/puma2.jpg",
+        description: "Puma Fashion",
+        price:  3500,
+        priceLabel: "₱ 3,500"
+    },
+    {
+        brandName: "World Balance",
+        imgSrc: "img/world2.jpg",
+        description: "Balance Fashion",
+        price:  3500,
+        priceLabel: "₱ 3,500"
+    },
+    {
+        brandName: "Nike",
+        imgSrc: "img/shoes7.png",
+        description: "AF7 low-top retro classic shoes",
+        price: 2100,
+        priceLabel: "₱ 2,100"
+    },
+    {
+        brandName: "Nike",
+        imgSrc: "img/shoes8.png",
+        description: "AF8 low-top retro classic shoes",
+        price: 2900,
+        priceLabel: "₱ 2,900"
+    },
+    
+    {
+        brandName: "Nike",
+        imgSrc: "img/shoes1.png",
+        description: "AF1 low-top retro classic shoes",
+        price:  2500,
+        priceLabel: "₱ 2,500"
+    },
+    {
+        brandName: "Adidas",
+        imgSrc: "img/adidas2.AVIF",
+        description: "Campus 00s Retro shoeses",
+        price:  1200,
+        priceLabel: "₱ 1,200"
+    },
+    
+    {
+        brandName: "Adidas",
+        imgSrc: "img/adidas4.AVIF",
+        description: "Campus 00s Retro shoeses",
+        price:  1400,
+        priceLabel: "₱ 1,400"
+    },
+    {
+        brandName: "Adidas",
+        imgSrc: "img/adidas5.jpg",
+        description: "Campus 00s Retro shoeses",
+        price:  1600,
+        priceLabel: "₱ 1,600"
+    },
+    {
+        brandName: "Adidas",
+        imgSrc: "img/adidas6.jpg",
+        description: "Campus 00s Retro shoeses",
+        price: 1700,
+        priceLabel: "₱ 1,700"
+    },
+    {
+        brandName: "Adidas",
+        imgSrc: "img/adidas7.jpg",
+        description: "Campus 00s Retro shoeses",
+        price:  1800,
+        priceLabel: "₱ 1,800"
+    },
+    {
+        brandName: "Adidas",
+        imgSrc: "img/adidas8.jpg",
+        description: "Campus 00s Retro shoeses",
+        price:  1900,
+        priceLabel: "₱ 1,900"
+    },
+    
+    
+    {
+        brandName: "Converse",
+        imgSrc: "img/converse3.jpg",
+        description: "Converse Fashion",
+        price:  2100,
+        priceLabel: "₱ 2,100"
+    },
+    {
+        brandName: "Converse",
+        imgSrc: "img/converse4.jpg",
+        description: "Converse Fashion",
+        price:  2700,
+        priceLabel: "₱ 2,700"
+    },
+    {
+        brandName: "Converse",
+        imgSrc: "img/converse5.jpg",
+        description: "Converse Fashion",
+        price:  3500,
+        priceLabel: "₱ 3,500"
+    },
+    {
+        brandName: "Converse",
+        imgSrc: "img/converse6.jpg",
+        description: "Converse Fashion",
+        price:  3300,
+        priceLabel: "₱ 3,300"
+    },
+    {
+        brandName: "Converse",
+        imgSrc: "img/converse7.jpg",
+        description: "Converse Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500"
+    },
+    {
+        brandName: "Converse",
+        imgSrc: "img/converse8.jpg",
+        description: "Converse Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500"
+    },
+    
+    
+    {
+        brandName: "Puma",
+        imgSrc: "img/puma3.jpg",
+        description: "Puma Fashion",
+        price:  3700,
+        priceLabel: "₱ 3,700"
+    },
+    {
+        brandName: "Puma",
+        imgSrc: "img/puma4.jpg",
+        description: "Puma Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500"
+    },
+    {
+        brandName: "Puma",
+        imgSrc: "img/puma5.jpg",
+        description: "Puma Fashion",
+        price:  2600,
+        priceLabel: "₱ 2,600"
+    },
+    {
+        brandName: "Puma",
+        imgSrc: "img/puma6.jpg",
+        description: "Puma Fashion",
+        price:  2100,
+        priceLabel: "₱ 2,100"
+    },
+    {
+        brandName: "Puma",
+        imgSrc: "img/puma7.jpg",
+        description: "Puma Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500"
+    },
+    {
+        brandName: "Puma",
+        imgSrc: "img/puma8.jpg",
+        description: "Puma Fashion",
+        price:  2800,
+        priceLabel: "₱ 2,800"
+    },
+    
+    
+    {
+        brandName: "World Balance",
+        imgSrc: "img/world3.jpg",
+        description: "Balance Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500"
+    },
+    {
+        brandName: "World Balance",
+        imgSrc: "img/world4.jpg",
+        description: "Balance Fashion",
+        price:  1500,
+        priceLabel: "₱ 1,500"
+    },
+    {
+        brandName: "World Balance",
+        imgSrc: "img/world5.jpg",
+        description: "Balance Fashion",
+        price:  2300,
+        priceLabel: "₱ 2,300"
+    },
+    {
+        brandName: "World Balance",
+        imgSrc: "img/world6.jpg",
+        description: "Balance Fashion",
+        price:  2500,
+        priceLabel: "₱ 2,500"
+    },
+];
 
 let filteredBrands = [];
 
@@ -103,23 +358,9 @@ function loadProducts() {
         filteredBrands = jsonData;
         filterBrand();
     } else {
-        fetch('http://127.0.0.1:5500/Shoes/products.txt')
-            .then((response) => {
-                if (!response.ok) {
-                throw new Error('Failed to fetch the file');
-                }
-                return response.text();
-            })
-            .then((data) => {
-                const jsonData = JSON.parse(data); // Parse as JSON
-                productItems = jsonData;
-                filteredBrands = jsonData;
-                localStorage.setItem('products', data);
-                filterBrand();
-            })
-            .catch((error) => {
-                console.error('Error accessing file:', error);
-            });
+        filteredBrands = productItems
+        localStorage.setItem("products", JSON.stringify(productItems));
+        filterBrand();
     }
 }
 
@@ -207,7 +448,6 @@ function increaseQuantity(quantity, index){
 function checkoutButton(){
     let checkout = document.getElementById('checkout')
     if (checkout){
-        window.location.href="http://127.0.0.1:5500/Shoes/feedback.html";
         alert("Thank you for checking out!");
         clearCart();
         closeBtn.click();
