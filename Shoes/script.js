843ec0f2f79aa5fc58eb219747e520ca583730ef
Paowish <paolocarunia139@gmail.com>
2024-12-05 21:12:40 +0800

    update all

--- Shoes/script.js
+++ Shoes/script.js
@@ -538,3 +538,12 @@ function register(){
     }
 }
 
+
+
+let profile = document.querySelector(".profile");
+let menu = document.querySelector(".menu");
+
+
+profile.addEventListener("click", ()=>{
+    menu.classList.toggle("active")
+})             
